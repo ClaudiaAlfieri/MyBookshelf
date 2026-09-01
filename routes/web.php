db5db5197df12a13307152827b61a430607e1d1d
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-09-01 12:20:28 +0100

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::view('/','home');
+
+Route::view('/books','books.index');
+
+Route::view('/genre','genre.index');
