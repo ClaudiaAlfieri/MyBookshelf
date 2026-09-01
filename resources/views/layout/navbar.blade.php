c9fa321caa84344dcba724be0fd8c42961ae0a67
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-09-01 12:20:01 +0100

    Crete layout.navbar.blade.php

---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-dark navbar-mybs shadow-sm sticky-top">
+    <div class="container">
+        <a class="navbar-brand" href="base.html">
+            <i class="bi bi-bookmark-star me-2"></i>
+            MyBookshelf
+        </a>
+
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#mainNavbar"
+            aria-controls="mainNavbar"
+            aria-expanded="false"
+            aria-label="Abrir navegação"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="mainNavbar">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" href="base.html">
+                        <i class="bi bi-house me-1"></i>
+                        Início
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="livros.html">
+                        <i class="bi bi-journal-bookmark me-1"></i>
+                        Meus Livros
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="generos.html">
+                        <i class="bi bi-tags me-1"></i>
+                        Géneros
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
