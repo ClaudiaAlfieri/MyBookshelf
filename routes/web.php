41fc6307de31440c5147ce925457b9f986fac87d
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-09-03 14:26:20 +0100

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::view('/', 'home')->name('home');
 
-Route::view('/','home');
+// Books
+Route::view('/books', 'books.index')->name('books.index');
+Route::view('/books/create', 'books.create')->name('books.create');
+Route::view('/books/edit', 'books.edit')->name('books.edit');
+Route::view('/books/{book}', 'books.show')->name('books.show');
 
-Route::view('/books','books.index');
-
-Route::view('/genres','genres.index');
+// Genres
+Route::view('/genres', 'genres.index')->name('genres.index');
+Route::view('/genres/create', 'genres.create')->name('genres.create');
+Route::view('/genres/edit', 'genres.edit')->name('genres.edit');
