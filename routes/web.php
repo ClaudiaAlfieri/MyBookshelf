557edf4bf7233d4e4fb66d747ce8edb822a492b8
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-09-07 21:21:05 +0100

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\GenreController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
 
 // Books
-Route::view('/books', 'books.index')->name('books.index');
-Route::view('/books/create', 'books.create')->name('books.create');
-Route::view('/books/edit', 'books.edit')->name('books.edit');
-Route::view('/books/{book}', 'books.show')->name('books.show');
+Route::resource('books', BookController::class);
 
 // Genres
-Route::view('/genres', 'genres.index')->name('genres.index');
-Route::view('/genres/create', 'genres.create')->name('genres.create');
-Route::view('/genres/edit', 'genres.edit')->name('genres.edit');
+Route::resource('genres', GenreController::class) ->except('show');
